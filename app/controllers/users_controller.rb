8f618858efd787ed382e4bad69f9dbbce850218a
yuki0427 <88xx.manu.xx88@gmail.com>
2020-01-08 01:45:37 +0000

    8.1.3終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
+      falsh[:success] = "ユーザー情報を更新しました"
+      redirect_to @user
     else
       render :edit
     end
